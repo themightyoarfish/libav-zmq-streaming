0be4fa4728cc929e2447b7b22d3c50022856ff09
Rasmus Diederichsen <rasmus@psiori.com>
2021-08-28 15:24:27 +0200

    use first arg as path

--- encode_video_zmq.cpp
+++ encode_video_zmq.cpp
@@ -12,34 +12,31 @@
 
 using namespace std;
 
-void stream_video() {
+int main(int argc, char* argv[]) {
+  /* av_log_set_level(AV_LOG_DEBUG); */
+  std::cout << "Libav version: " << av_version_info() << std::endl;
 
   AVTransmitter transmitter("*", 15001, 15);
 
-  std::vector<std::string> filenames;
-  cv::glob("/Users/rasmus/Downloads/images/*.jpeg", filenames);
-  std::sort(filenames.begin(), filenames.end());
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <directory>" << std::endl;
+    std::exit(1);
+  }
+  const string directory = argv[1];
+  vector<string> filenames;
+  cv::glob(directory + "*.jpeg", filenames);
+  sort(filenames.begin(), filenames.end());
 
   const int n_frames = filenames.size();
   int ms = 0;
   for (int i = 0; i < n_frames; ++i) {
 
     cv::Mat image = cv::imread(filenames[i]);
-    auto tic = std::chrono::system_clock::now();
+    auto tic = chrono::system_clock::now();
     transmitter.encode_frame(image);
-    auto toc = std::chrono::system_clock::now();
-    ms += std::chrono::duration_cast<std::chrono::milliseconds>(toc - tic)
-              .count();
-    /* std::cout << "fps = " << 1 / ((ms / 1000.0) / (i + 1)) << std::endl; */
+    auto toc = chrono::system_clock::now();
+    ms += chrono::duration_cast<chrono::milliseconds>(toc - tic).count();
+    /* cout << "fps = " << 1 / ((ms / 1000.0) / (i + 1)) << endl; */
   }
-}
-
-int main() {
-  /* av_log_set_level(AV_LOG_DEBUG); */
-  /* av_register_all(); */
-  std::cout << "Libav version: " << av_version_info() << std::endl;
-
-  stream_video();
-
   return 0;
 }
