ac0288582053b0b30c6c226cf038a901a8f60104
Rasmus Diederichsen <rasmus@psiori.com>
2021-08-28 10:37:32 +0200

    Fix makefile

--- decode_video_zmq.cpp
+++ decode_video_zmq.cpp
@@ -1,9 +1,9 @@
 #include "avreceiver.hpp"
-int main(int argc, char *argv[])
-{
-    AVReceiver receiver("localhost", 15001);
-    while (true) {
-        receiver.receive();
-    }
-    return 0;
+int main(int argc, char *argv[]) {
+  const std::string host = argc > 1 ? argv[1] : "localhost";
+  AVReceiver receiver(host, 15001);
+  while (true) {
+    receiver.receive();
+  }
+  return 0;
 }
