d17537035b70a25f7e1cd4562d0875873718ec71
joelosw <joel.oswald@psiori.com>
2022-08-10 16:49:33 +0200

    format & -Wparentheses fix

--- avutils.cpp
+++ avutils.cpp
@@ -25,13 +25,18 @@ std::string av_strerror2(int errnum) {
   return std::string(v.begin(), v.end());
 }
 
-int initialize_avformat_context(AVFormatContext *&fctx, AVOutputFormat *format,
-                                const char *out_file) {
+int initialize_avformat_context(AVFormatContext*& fctx,
+                                AVOutputFormat* format,
+                                const char* out_file) {
   return avformat_alloc_output_context2(&fctx, format, format->name, out_file);
 }
 
-void set_codec_params(AVCodecContext *&codec_ctx, double width, double height,
-                      int fps, int target_bitrate, int gop_size) {
+void set_codec_params(AVCodecContext*& codec_ctx,
+                      double width,
+                      double height,
+                      int fps,
+                      int target_bitrate,
+                      int gop_size) {
   const AVRational dst_fps = {fps, 1};
 
   codec_ctx->codec_tag = 0;
@@ -39,20 +44,21 @@ void set_codec_params(AVCodecContext *&codec_ctx, double width, double height,
     codec_ctx->bit_rate = target_bitrate;
   }
   codec_ctx->thread_count = 1;
-  codec_ctx->codec_id = AV_CODEC_ID_VP9;
-  codec_ctx->codec_type = AVMEDIA_TYPE_VIDEO;
-  codec_ctx->width = width;
-  codec_ctx->height = height;
-  codec_ctx->gop_size = gop_size;
-  codec_ctx->pix_fmt = AV_PIX_FMT_YUV420P;
-  codec_ctx->framerate = dst_fps;
-  codec_ctx->time_base = av_inv_q(dst_fps);
+  codec_ctx->codec_id     = AV_CODEC_ID_VP9;
+  codec_ctx->codec_type   = AVMEDIA_TYPE_VIDEO;
+  codec_ctx->width        = width;
+  codec_ctx->height       = height;
+  codec_ctx->gop_size     = gop_size;
+  codec_ctx->pix_fmt      = AV_PIX_FMT_YUV420P;
+  codec_ctx->framerate    = dst_fps;
+  codec_ctx->time_base    = av_inv_q(dst_fps);
   /* codec_ctx->thread_type = FF_THREAD_SLICE; */
 }
 
-int initialize_codec_stream(AVStream *&stream, AVCodecContext *&codec_ctx,
-                            AVCodec *&codec) {
-  AVDictionary *codec_options = nullptr;
+int initialize_codec_stream(AVStream*& stream,
+                            AVCodecContext*& codec_ctx,
+                            AVCodec*& codec) {
+  AVDictionary* codec_options = nullptr;
   /* av_dict_set(&codec_options, "profile", "high", 0); */
   /* av_dict_set(&codec_options, "preset", "ultrafast", 0); */
   /* av_dict_set(&codec_options, "tune", "zerolatency", 0); */
@@ -74,9 +80,9 @@ int initialize_codec_stream(AVStream *&stream, AVCodecContext *&codec_ctx,
     /* std::cout << "No Extradata present in AVFormatContext" << std::endl; */
   }
 
-  bool all_found = true;
-  AVDictionaryEntry *e = nullptr;
-  while (e = av_dict_get(codec_options, "", e, AV_DICT_IGNORE_SUFFIX)) {
+  bool all_found       = true;
+  AVDictionaryEntry* e = nullptr;
+  while ((e = av_dict_get(codec_options, "", e, AV_DICT_IGNORE_SUFFIX))) {
     /* std::cout << "Did not find option " << e->key << ": " << e->value */
     /*           << std::endl; */
     all_found = false;
@@ -89,32 +95,34 @@ int initialize_codec_stream(AVStream *&stream, AVCodecContext *&codec_ctx,
   return ret;
 }
 
-SwsContext *initialize_sample_scaler(AVCodecContext *codec_ctx, double width,
+SwsContext* initialize_sample_scaler(AVCodecContext* codec_ctx,
+                                     double width,
                                      double height) {
-  SwsContext *swsctx = sws_getContext(width, height, AV_PIX_FMT_RGB24, width,
+  SwsContext* swsctx = sws_getContext(width, height, AV_PIX_FMT_RGB24, width,
                                       height, codec_ctx->pix_fmt, SWS_BICUBIC,
                                       nullptr, nullptr, nullptr);
   return swsctx;
 }
 
-AVFrame *allocate_frame_buffer(AVCodecContext *codec_ctx, double width,
-                               double height) {
-  AVFrame *frame = av_frame_alloc();
+AVFrame*
+allocate_frame_buffer(AVCodecContext* codec_ctx, double width, double height) {
+  AVFrame* frame = av_frame_alloc();
 
-  std::uint8_t *framebuf = new uint8_t[av_image_get_buffer_size(
+  std::uint8_t* framebuf = new uint8_t[av_image_get_buffer_size(
       codec_ctx->pix_fmt, width, height, 1)];
   av_image_fill_arrays(frame->data, frame->linesize, framebuf,
                        codec_ctx->pix_fmt, width, height, 1);
-  frame->width = width;
+  frame->width  = width;
   frame->height = height;
   frame->format = static_cast<int>(codec_ctx->pix_fmt);
-  frame->pts = 0;
+  frame->pts    = 0;
 
   return frame;
 }
 
-int write_frame(AVCodecContext *codec_ctx, AVFormatContext *fmt_ctx,
-                AVFrame *frame) {
+int write_frame(AVCodecContext* codec_ctx,
+                AVFormatContext* fmt_ctx,
+                AVFrame* frame) {
   AVPacket pkt = {0};
   /* av_init_packet(&pkt); */
 
@@ -126,7 +134,8 @@ int write_frame(AVCodecContext *codec_ctx, AVFormatContext *fmt_ctx,
 
   ret = avcodec_receive_packet(codec_ctx, &pkt);
   if (ret < 0) {
-    /* std::cout << "Error receiving packet from codec context!" << std::endl; */
+    /* std::cout << "Error receiving packet from codec context!" << std::endl;
+     */
     return ret;
   }
 
@@ -135,33 +144,33 @@ int write_frame(AVCodecContext *codec_ctx, AVFormatContext *fmt_ctx,
   return ret;
 }
 
-void generatePattern(cv::Mat &image, unsigned char i) {
+void generatePattern(cv::Mat& image, unsigned char i) {
   image.setTo(cv::Scalar(255, 255, 255));
   float perc_height = 1.0 * i / 255;
-  float perc_width = 1.0 * i / 255;
+  float perc_width  = 1.0 * i / 255;
   image.row(perc_height * image.rows).setTo(cv::Scalar(0, 0, 0));
   image.col(perc_width * image.cols).setTo(cv::Scalar(0, 0, 0));
 }
 
-cv::Mat avframeYUV402p2Mat(const AVFrame *frame) {
+cv::Mat avframeYUV402p2Mat(const AVFrame* frame) {
   std::vector<int> sizes = {frame->height, frame->width};
   std::vector<size_t> steps{static_cast<size_t>(frame->linesize[0])};
 
-  const auto height = frame->height;
-  const auto width = frame->width;
+  const auto height      = frame->height;
+  const auto width       = frame->width;
   const auto actual_size = cv::Size(width, height);
-  void *y = frame->data[0];
-  void *u = frame->data[1];
-  void *v = frame->data[2];
+  void* y                = frame->data[0];
+  void* u                = frame->data[1];
+  void* v                = frame->data[2];
 
   cv::Mat y_mat(height, width, CV_8UC1, y, frame->linesize[0]);
   cv::Mat u_mat(height / 2, width / 2, CV_8UC1, u, frame->linesize[1]);
   cv::Mat v_mat(height / 2, width / 2, CV_8UC1, v, frame->linesize[2]);
   cv::Mat u_resized, v_resized;
   cv::resize(u_mat, u_resized, actual_size, 0, 0,
-             cv::INTER_NEAREST); // repeat u values 4 times
+             cv::INTER_NEAREST);  // repeat u values 4 times
   cv::resize(v_mat, v_resized, actual_size, 0, 0,
-             cv::INTER_NEAREST); // repeat v values 4 times
+             cv::INTER_NEAREST);  // repeat v values 4 times
 
   cv::Mat yuv;
 
@@ -172,4 +181,4 @@ cv::Mat avframeYUV402p2Mat(const AVFrame *frame) {
   cv::cvtColor(yuv, image, cv::COLOR_YUV2RGB);
   return image;
 }
-} // namespace avutils
+}  // namespace avutils
